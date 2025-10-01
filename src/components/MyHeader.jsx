a8921fe7a5ac376692bbb453d4ad7be1462da28d
SamirSadriu-24 <220008833+SamirSadriu-24@users.noreply.github.com>
2025-10-01 14:22:57 +0200

    IMPORTAZIONE FILE E HEADER

---
+++ src/components/MyHeader.jsx
@@ -0,0 +1,10 @@
+const MyHeader = () => {
+    return (
+        <div className="header">
+            <h1>SAMFLIX</h1>
+            <p>Scegli il genere che vuoi, ti daremo le opzioni che abbiamo!</p>
+        </div>
+    )
+}
+
+export default MyHeader
